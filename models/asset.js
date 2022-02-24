08cdc2a0adec4d0b297911e44e0eafb00145bf8c
Your Name <charlesjohnstoncpa@gmail.com>
2022-02-24 13:12:22 -0700

    models

---
+++ models/asset.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const partsSchema = mongoose.Schema({
+  description: String,
+  quantity: Number,
+  photoUrl: String
+})
+
+const serviceSchema = mongoose.Schema({
+  date: Date,
+  description: String,
+  mileage: Number,
+  nextServiceDue: Date,
+  photoUrl: String
+  })
+
+const assetSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  type: String,
+  year: Number,
+  make: String,
+  model: String,
+  description: String,
+  inServiceDate: Date,
+  beginningMileage: Number,
+  currentMileage: Number,
+  photoUrl: String,
+  service: [serviceSchema],
+  parts: [partsSchema]
+})
+
+module.exports = mongoose.model('Asset', assetSchema);
--- models/user.js
+++ models/user.js
@@ -5,7 +5,10 @@ const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema({
   username: {type: String, required: true, lowercase: true, unique: true},
+  name: {type: String, required: true, unique: true},
   email: {type: String, required: true, lowercase: true, unique: true},
+  phone: {type: String, required: true, unique: true},
+  smsNotifications: {type: Boolean, required: true},
   password: String,
   photoUrl: String  // string from aws!
 }, {
